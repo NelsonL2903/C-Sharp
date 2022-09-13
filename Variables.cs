724db80b13cca01980c7a8369bfbc10009b5d3ae
Nelson Loop <nelsonploop@gmail.com>
2022-09-12 23:53:24 -0400

    Created program to print out variables.

---
+++ Variables.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Variables
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Create Variables
+      string name = "Shadow";
+      string breed = "Golden Retriever";
+      int age = 5;
+      double weight = 65.22;
+      bool spayed = true;
+
+      // Print variables to the console
+      Console.WriteLine("Name: " + name);
+      Console.WriteLine("Breed: " + breed);
+      Console.WriteLine("Age: " + age);
+      Console.WriteLine("Weight: " + weight);
+      Console.WriteLine("Spayed: " + spayed);
+
+    }
+  }
+}
